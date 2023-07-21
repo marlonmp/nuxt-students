b97857bdcbab34a76632447cee6ae400fd1665e1
marlon.mosquera <marlon.mosquera@i2d.net.co>
2023-07-21 18:02:58 -0500

    eslint, tailwind and nuxt config added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,57 @@
+import { fileURLToPath } from 'url';
+import type { Config as tailwindConfig } from 'tailwindcss';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  typescript: { strict: true },
+
+  runtimeConfig: {
+    MONGO_URI: process.env.MONGO_URI
+  },
+
+  alias: {
+    '@types': fileURLToPath(new URL('./types', import.meta.url)),
+    '@models': fileURLToPath(new URL('./models', import.meta.url)),
+    '@validators': fileURLToPath(new URL('./validators', import.meta.url))
+  },
+
+  css: [
+    '@/assets/css/main.css'
+  ],
+
+  modules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode'
+  ],
+
+  eslint: {
+    cache: false
+  },
+
   vite: {
     server: {
-      hmr: {
-        protocol: 'ws',
-        host: '0.0.0.0'
-      }
+      hmr: { protocol: 'ws', host: '0.0.0.0', port: 3001 }
     }
+  },
+
+  nitro: {
+    plugins: [
+      '~/server/db/mongodb.ts'
+    ]
+  },
+
+  tailwindcss: {
+    cssPath: '~/assets/css/main.css',
+    config: <Partial<tailwindConfig>> {
+      darkMode: 'class',
+      configPath: '~/config/tailwind.ts'
+    }
+  },
+
+  colorMode: {
+    preference: 'system',
+    fallback: 'light'
   }
-})
+});
